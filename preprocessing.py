43fdac682caf1a64eb3a7df5212d674d21ec5192
Shan Dou <shandou.seismo@gmail.com>
2018-07-06 12:19:17 -0700

    more feature engineering

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import subprocess
+import itertools
 import numpy as np
 import pandas as pd
 
@@ -63,9 +64,121 @@ def csv_list_fields(csv_in=''):
     ).decode('utf8').strip().split(',')
 
 
-def mapper_category2woe(df, col=''):
-    p_ref = df['is_attributed'].sum() / len(df)
-    prob_df = pd.DataFrame(df.groupby([col])['is_attributed'].mean())
-    prob_df['ratio'] = prob_df.is_attributed / p_ref
-    return prob_df['ratio'].to_dict()
+def mapper_label2count(df, col=''):
+    '''
+    Replacing high-cardinality categorical labels with normalized risk factor
+    normalized risk factor = target mean of each label normalized by the
+    grand target mean of all labels
+    Mapping should be generated with training data, and then apply the
+    resulting mapping to testing data (no testing data snooping!)
 
+    Parameters
+    ------------
+    df : pandas.DataFrame
+        Input data frame to be processed; prediction target must be included
+    col : str
+        Feature name to be processed
+
+    Returns
+    ----------
+    mapper : dict
+        Dictionary with mapping from labels to risk factor values
+
+    Examples
+    ---------
+    Process 'ip' field of the input dataframe
+
+    >>> mapper = preprocessing.mapper_label2riskfactor(df, col='ip')
+    >>> df[col + '_risk'] = df[col].map(mapper)
+    '''
+    return df[col].value_counts().to_dict()
+
+
+def mapper_label2riskfactor(df, col=''):
+    '''
+    Replacing high-cardinality categorical labels with normalized risk factor
+    normalized risk factor = target mean of each label normalized by the
+    grand target mean of all labels
+    Mapping should be generated with training data, and then apply the
+    resulting mapping to testing data (no testing data snooping!)
+
+    Parameters
+    ------------
+    df : pandas.DataFrame
+        Input data frame to be processed; prediction target must be included
+    col : str
+        Feature name to be processed
+
+    Returns
+    ----------
+    mapper : dict
+        Dictionary with mapping from labels to risk factor values
+
+    Examples
+    ---------
+    Process 'ip' field of the input dataframe
+
+    >>> mapper = preprocessing.mapper_label2riskfactor(df, col='ip')
+    >>> df[col + '_risk'] = df[col].map(mapper)
+    '''
+    p_normalization = df['is_attributed'].mean()
+    prob_df = pd.DataFrame(df.groupby([col])['is_attributed'].mean())
+    prob_df['risk'] = prob_df.is_attributed / p_normalization
+    return prob_df['risk'].to_dict()
+
+def df_rarelabel_imputer(
+    df, cols=[], thresh_percentage=1.0, replace_with=1e10
+):
+    '''
+    Replace rare labels with dummy value. Critical for avoiding overfitting
+
+    Parameters
+    ------------
+    df : pandas.DataFrame
+        Input dataframe to be processed
+    cols : list
+        list of column names to be processed
+    thresh_percentage : float
+        Threshold for defining rare labels
+    replace_with : float
+        Dummy value used for busketing all rare labels to the same group
+        Should be a value not present in any raw labels
+
+    Returns
+    ----------
+    df : pandas.DataFrame
+        Output dataframe whose rare labels have been imputed
+
+    Examples
+    ---------
+    Replace values in ['ip', 'app', 'channel'] that are only present in less
+    than 1% of the observations with a large number 1e10
+
+    >>> df = impute_rare_label(
+    ... df, cols=['ip', 'app', 'channel'], thresh_percentage=1.0,
+    ... replace_with=1e10
+    ... )
+    '''
+    # Turn off SettingWithCopyWarning from pandas
+    pd.options.mode.chained_assignment = None
+    for col in cols:
+        # Compute proportion of all value counts in percentage
+        df_temp = df[col].value_counts() / len(df) * 100.0
+        labels_rare = df_temp[df_temp <= thresh_percentage].index
+        if len(labels_rare) > 0:
+            # Impute rare labels; keep other labels intact
+            df[col] = np.where(
+                df[col].isin(labels_rare), replace_with, df[col]
+            )
+    return df
+
+
+def df_feature_grouper(df, cols=[], r=2):
+    feature_groups = itertools.combinations(cols, r)
+    for group in feature_groups:
+        name = 'count_{}'.format('-'.join(group))
+        df[name] = df.merge(
+            df.groupby(list(group)).count()['is_attributed'].reset_index(),
+            how='left', on=list(group)
+        )['is_attributed_y']
+    return df.fillna(0)
